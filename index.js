5a0b87d31f588a52037b0e2985f4257e4b95a491
irina-cebotari <irina.cebotari0301@gmail.com>
2024-03-13 20:22:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,124 @@
+
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i, 'hi')
+// }
+
+// for(;;) {
+//     console.log('hi')
+// }
+
+// for(let i = 10; i >= 1; i--) console.log('hi', i)
+
+// for(let i = 10; i >= 1; i-=2) console.log('hi', i)
+
+
+// let i = 10;
+
+// while(i >= 0) {
+//     console.log('hi', i)
+//     i--
+// }
+
+// while(--i) {
+//     console.log('hi', i)
+// }
+
+// let i = 10;
+
+// do {
+//     console.log('hi', i)
+// } while (i > 20)
+
+
+
+// console.log(value)
+// var value = 5;
+
+
+// console.log(value)
+
+// let value = 5;
+
+// console.log(value)
+
+
+// var i = 10;
+
+// function func() {
+//     var i = 20
+//     console.log(i)
+    
+//     function func1() {
+//         var i
+//         console.log(i)
+//         i = 15
+//     }
+//     func1()
+//     console.log(i)
+// }
+
+// func()
+
+
+// const condition = true
+// if(condition) {
+//     var i = 10
+// }
+
+// console.log(i)
+
+// const condition = true
+// if(condition) {
+//     let i = 10
+// }
+
+// console.log(i)
+
+
+
+// const arrowFunc = (nume) => {
+//     return 'Hi ' + nume
+// }
+
+// const arrowFunc = (nume) => return 'Hi ' + nume
+
+// const arrowFunc = (nume) => 'Hi ' + nume
+
+// const arrowFunc = nume => 'Hi ' + nume
+
+// console.log(arrowFunc('Dima'))
+
+
+// console.log(funcDeclaration('Dima', 'Diana', 'Mam'))
+// function funcDeclaration(nume) {
+//     console.log(arguments)
+//     return 'Hi ' + nume
+// }
+
+// console.log(funcDeclaration('Dima', 'Diana', 'Mam'))
+
+
+// const funcExpression = function (nume) {
+//     console.log(arguments)
+//     return 'hi ' + nume
+//     console.log('jhfgcdbgvkjhbsdf')
+// }
+
+// console.log(funcExpression('Dima'))
+
+
+// const condition = true
+
+// const funcExpression = function (nume) {
+//     if(condition) {
+//         return 'first case'
+//     } else {
+//         return 'second case'
+//         console.log('after')
+//     }
+// }
+
+// const funcResult = funcExpression()
+
+// console.log(funcResult)
